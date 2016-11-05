bff8ea4c767f32c03904ec22c4988f8a5ca5aa12
Hauke Bartsch <hbartsch@ucsd.edu>
2016-11-04 19:06:59 -0700

    first working test of script

--- putDataIntoREDCap.php
+++ putDataIntoREDCap.php
@@ -16,43 +16,101 @@ if (!is_readable($options['f'])) {
     exit(1);
 }
 
-$data = json_decode(file_get_contents($options['f']),true);
+$file_contents = json_decode(file_get_contents($options['f']),true);
 
 $token = "";
 $tokens = json_decode(file_get_contents("tokens.json"), true);
-$token = $tokens[$data['lmt_site']];
+$token = $tokens[$file_contents['lmt_site']];
 // put your token in here
-$token = "TOKENGOHERE";
 
-$participant = $data['lmt_subject_id'];
-$event_name  = $data['lmt_event_name'];
+$participant = $file_contents['lmt_subject_id'];
+$event_name  = $file_contents['lmt_event_name'];
 $pGUID = $participant;
 
 // send over one data set
 // create loop over all data entries in $data['data']
 
-// add tests for subject does not exist,
 // copy values after successful import to data-import-archive and data-import-fail
 
-foreach ( $data['data'] as $entry ) {
+$data = array(
+    'token' => $token,
+    'content' => 'record',
+    'format' => 'json',
+    'type' => 'flat',
+    'records' => array($participant),
+    'fields' => array('cp_consent_sign_v2'),
+    'events' => array('baseline_year_1_arm_1'),
+    'rawOrLabel' => 'raw',
+    'rawOrLabelHeaders' => 'raw',
+    'exportCheckboxLabel' => 'false',
+    'exportSurveyFields' => 'false',
+    'exportDataAccessGroups' => 'false',
+        'returnFormat' => 'json'
+);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://abcd-rc.ucsd.edu/redcap/api/');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
+$output = curl_exec($ch);
+curl_close($ch);
 
-  $payload = array( "id_redcap" => $pGUID,
-  "redcap_event_name" => $event_name,
-  //"mrif_score" => $score,
-  //"mrif_hydrocephalus" => $hydrocephalus,
-  //"mrif_herniation" => $herniation,
-  //"mrif_other_notes" => $other_notes,
-  //"mrif_score_copy" => $score,
-  //"mrif_hydrocephalus_copy" => $hydrocephalus,
-  //"mrif_herniation_copy" => $herniation,
-  //"mrif_other_notes_copy" => $other_notes,
-  //"mrif_scan_read_dte" => date("Y-m-d"));
-  //if ($score > 2) {
-      // also mark the date a first note was send
-      //$payload["mrif_not_dte"] = date("Y-m-d");
-  //}
-  );
+// check for returned value of a real participant (test with fake name)
+$retval = json_decode($output, true);
+$ok = false;
+$consent = $retval[0];
+if($consent["cp_consent_sign_v2"]=="1"){
+    $ok = true;
+}
+if (!$ok) {
+    echo ("Error: could not import, participant ".$participant. " does not exist\n");
+    exit(1);
+}
 
+
+
+// add some counters, how many errors?
+$num_attempted = 0;
+$num_successful = 0;
+$num_failed = 0;
+
+$payload = array( "id_redcap" => $pGUID,
+                  "redcap_event_name" => $event_name,
+);
+
+foreach ( $file_contents as $k => $entry ) {
+
+  //print_r($k." => ".$entry." /////\n");
+  // string entries are single data points about the task 
+  if(is_string($entry)){
+
+    // remove this if statement once instruments.csv is updated
+    if($k == "lmt_user") break;
+    $payload[$k] = $entry;
+    $num_attempted++;
+  }
+  // the associative array 'data' containings the trials is handled here
+  else{
+    //for each trial aka slide recorded
+    $i = 0;
+    foreach ($entry as $kay => $trial) {
+      // for each data point of each slide
+      foreach ($trial as $key => $value) {
+        $payload[$key . sprintf("_%02d", $i)] = $value;
+        $num_attempted++;
+      }
+      $i++;
+    }
+  }
+}
+//print_r($payload);
+if(1){
   $data = array(
       'token'             => $token,
       'content'           => 'record',
@@ -75,9 +133,14 @@ foreach ( $data['data'] as $entry ) {
   curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
   curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
   $output = curl_exec($ch);
+  echo($output);
   curl_close($ch);
 }
 
+
+// print out how many entries could be imported
+echo("\n---Script Results---\nnumber of attempted entries: ".$num_attempted."\n");
+//echo($num_successful." entries successfully imported. ".$num_failed." entries failed.\n");
 
 
 
